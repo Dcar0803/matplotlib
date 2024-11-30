13cdf093a2885ea3eb558a83ee7196f9a441ab51
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-29 20:45:26 -0500

    Test: Created a test function for ploting a line given the y-intercept, the slope, the lower x boundary, and the upper x boundary

--- test.py
+++ test.py
@@ -9,3 +9,12 @@ def test_plot_normal_distribution():
         print("Test for normal distribution passed")
     except Exception as e:
         print(f"Test for plot_normal_distribution failed: {e}")
+
+
+def test_plot_line():
+    try:
+        plot_line(y_intercept=1, slope=2, x_min=-10, x_max=10)
+        print("Test for plot_line passed.")
+    except Exception as e:
+        print(f"Test for plot_line failed: {e}")
+    
