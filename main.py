38feeefc50860b614831135a20e45c66c019afbd
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-29 20:34:18 -0500

    Function: Created function that plot 200 points sampled from a standard normal distribution

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+def plot_normal_distribution():
+    data = np.random.normal(size=200)
+    plt.plot(data, 'o')
+    plt.title("Standard Normal Distribution")
+    plt.xlabel("Index")
+    plt.ylabel("Value")
+    plt.grid(color = 'green')
+    plt.show()
+    return
