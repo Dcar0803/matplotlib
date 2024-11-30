e826e8bd948df29e48fd74facad4f506638746ce
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-29 20:36:17 -0500

    Doc: Created documentation for the plot_normal_distribution function

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_normal_distribution():
+
+    """Plots 200 points sampled from a standard normal distribution.
+    
+    The points are displayed as a scatter plot where the x-axis is the index of the point,
+    and the y-axis is the sampled value.
+    """
     data = np.random.normal(size=200)
     plt.plot(data, 'o')
     plt.title("Standard Normal Distribution")
