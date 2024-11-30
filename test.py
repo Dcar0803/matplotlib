60266a0606d409b03c227f6282349dfd44fe9cec
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-29 20:18:16 -0500

    Test: Created test function for plotting 200 points sampled from a standard normal distribution

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def test_plot_normal_distribution():
+
+    try:
+        plot_normal_distribution()
+        print("Test for normal distribution passed")
+    except:
+        print(f"Test for plot_normal_distribution failed: {e}")
