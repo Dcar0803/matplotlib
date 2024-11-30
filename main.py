d094dfb6d748210e9a3abe21b2b15734e65a3742
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-29 20:57:58 -0500

    Doc: Created documentation for the plot_line() function

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ def plot_normal_distribution():
     plt.show()
     
 def plot_line(y_intercept, slope, x_min, x_max):
+
+    """Plots a line based on the formula y = mx + b in a specified range.
+    
+    Parameters:
+        y_intercept (float): The y-intercept of the line.
+        slope (float): The slope of the line.
+        x_min (float): The starting x value for the plot.
+        x_max (float): The ending x value for the plot.
+        
+    The function plots the line within the range defined by x_min and x_max.
+    """
     
     x = np.linspace(x_min, x_max, 400)
     y = (slope * x) + y_intercept
